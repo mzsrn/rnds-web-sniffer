42e6476ff845bb0389241582bd692759ff00be23
Marat Zasorin <mzasorin@rnds.pro>
2020-06-16 12:45:42 +0300

    add broker type

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem "slim-rails"
 gem 'font_awesome5_rails'
+gem 'enumerize'
 
 gem 'devise'
 gem 'cancancan'
--- app/models/resource_setting.rb
+++ app/models/resource_setting.rb
@@ -1,6 +1,12 @@
 class ResourceSetting < ApplicationRecord
+  
+  extend Enumerize
+
   self.inheritance_column = :foo
 
   has_one :portfolio
   belongs_to :user
+
+  enumerize :type, in: [:finam, :tinkoff]
+
 end
